5fa3761185dd0426cc09b3737d7b8312b304f1d5
lemokami <jesswinw@gmail.com>
2022-04-14 14:54:06 +0530

    feat: add redirect to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 import PrivateRoute from '../hoc/PrivateRoute';
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/create');
+  });
   return <PrivateRoute>fdksj</PrivateRoute>;
 };
 
