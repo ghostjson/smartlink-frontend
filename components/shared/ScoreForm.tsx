e42c7ca418264d9a0c1af12a280e35e4b3d98bc4
lemokami <jesswinw@gmail.com>
2022-05-11 01:43:39 +0530

    feat: add score form

---
+++ components/shared/ScoreForm.tsx
@@ -0,0 +1,154 @@
+import { FC } from 'react';
+import { BiTrash } from 'react-icons/bi';
+import TextInput from './TextInput';
+
+interface ScoreFormProps {
+  title: string;
+  total: number;
+}
+const ScoreForm: FC<ScoreFormProps> = ({ title, total }) => {
+  return (
+    <div className='flex flex-col items-center'>
+      <h1 className='font-bold text-2xl py-2 capitalize'>{title}</h1>
+      <div className='border border-slate-200 shadow-xs p-4 rounded-lg my-2 '>
+        <div className='my-2'>
+          <span>Results based on score:</span>
+          <div className='flex flex-col w-full space-y-6'>
+            {/* mcq option  */}
+            <div className='flex flex-col justify-evenly'>
+              <span className='flex w-full items-center space-x-2'>
+                <span>A) </span>
+                <TextInput name='a' placeholder='Enter an Option' />
+              </span>
+
+              <div className='flex'>
+                <span className='flex w-full items-center ml-2 space-x-2'>
+                  <span>From </span>
+                  <input
+                    type='number'
+                    name='a'
+                    placeholder='0'
+                    max={total}
+                    className='input input-bordered w-full'
+                  />
+                </span>
+                <span className='flex w-full items-center ml-2 space-x-2'>
+                  <span>To </span>
+                  <input
+                    type='number'
+                    name='a'
+                    placeholder={'' + total}
+                    max={total}
+                    className='input input-bordered w-full'
+                  />
+                </span>
+              </div>
+            </div>
+            {/* end mcq option  */}
+
+            {/* mcq option  */}
+            <div className='flex flex-col justify-evenly'>
+              <span className='flex w-full items-center space-x-2'>
+                <span>B) </span>
+                <TextInput name='a' placeholder='Enter an Option' />
+              </span>
+
+              <div className='flex'>
+                <span className='flex w-full items-center ml-2 space-x-2'>
+                  <span>From </span>
+                  <input
+                    type='number'
+                    name='b'
+                    placeholder='0'
+                    max={total}
+                    className='input input-bordered w-full'
+                  />
+                </span>
+                <span className='flex w-full items-center ml-2 space-x-2'>
+                  <span>To </span>
+                  <input
+                    type='number'
+                    name='b'
+                    placeholder={'' + total}
+                    max={total}
+                    className='input input-bordered w-full'
+                  />
+                </span>
+              </div>
+            </div>
+            {/* end mcq option  */}
+            {/* mcq option  */}
+            <div className='flex flex-col justify-evenly'>
+              <span className='flex w-full items-center space-x-2'>
+                <span>C) </span>
+                <TextInput name='a' placeholder='Enter an Option' />
+              </span>
+
+              <div className='flex'>
+                <span className='flex w-full items-center ml-2 space-x-2'>
+                  <span>From </span>
+                  <input
+                    type='number'
+                    name='c'
+                    placeholder='0'
+                    max={total}
+                    className='input input-bordered w-full'
+                  />
+                </span>
+                <span className='flex w-full items-center ml-2 space-x-2'>
+                  <span>To </span>
+                  <input
+                    type='number'
+                    name='c'
+                    placeholder={'' + total}
+                    max={total}
+                    className='input input-bordered w-full'
+                  />
+                </span>
+              </div>
+            </div>
+            {/* end mcq option  */}
+            {/* mcq option  */}
+            <div className='flex flex-col justify-evenly'>
+              <span className='flex w-full items-center space-x-2'>
+                <span>D) </span>
+                <TextInput name='a' placeholder='Enter an Option' />
+              </span>
+
+              <div className='flex'>
+                <span className='flex w-full items-center ml-2 space-x-2'>
+                  <span>From </span>
+                  <input
+                    type='number'
+                    name='d'
+                    placeholder='0'
+                    max={total}
+                    className='input input-bordered w-full'
+                  />
+                </span>
+                <span className='flex w-full items-center ml-2 space-x-2'>
+                  <span>To </span>
+                  <input
+                    type='number'
+                    name='d'
+                    placeholder={'' + total}
+                    max={total}
+                    className='input input-bordered w-full'
+                  />
+                </span>
+              </div>
+            </div>
+            {/* end mcq option  */}
+          </div>
+
+          <label className='flex items-center gap-2 mt-6 cursor-pointer'>
+            <input type='checkbox' className='checkbox checkbox-primary ' />
+            <span>Show results</span>
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ScoreForm;
--- pages/create/forms/quiz.tsx
+++ pages/create/forms/quiz.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uniqid from 'uniqid';
 
 import HomeLayout from '../../../components/layout/Home.layout';
 import QuizForm from '../../../components/shared/QuizForm';
+import ScoreForm from '../../../components/shared/ScoreForm';
 import PrivateRoute from '../../../hoc/PrivateRoute';
 
 const Quiz = () => {
+  const [page, setPage] = useState(0);
+  const moveToNextPage = () => {
+    if (page >= 1) return;
+    setPage((page) => page + 1);
+  };
+  const moveToPreviousPage = () => {
+    if (page < 1) return;
+    setPage((page) => page - 1);
+  };
+
   return (
     <PrivateRoute>
       <HomeLayout>
         <ul className='steps w-full my-4'>
           <li className='step step-primary'>Create</li>
-          <li className='step'>Score</li>
+          <li className={`step ${page === 1 && 'step-primary'}`}>Score</li>
           <li className='step'>Design</li>
         </ul>
         <div className='flex flex-col space-y-5 items-center'>
-          <QuizForm />
+          {page === 0 ? <QuizForm /> : <ScoreForm title='apple' total={100} />}
+
           <div className='flex gap-2'>
-            <button className={`btn btn-primary xl:btn-wide `}>Back</button>
-            <button className='btn btn-primary xl:btn-wide'>Next</button>
+            <button
+              onClick={moveToPreviousPage}
+              className={`btn btn-primary xl:btn-wide `}>
+              Back
+            </button>
+            <button
+              onClick={moveToNextPage}
+              className='btn btn-primary xl:btn-wide'>
+              Next
+            </button>
           </div>
         </div>
       </HomeLayout>
