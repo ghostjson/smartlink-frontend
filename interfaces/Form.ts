01aaa5f850d4ee47e082e3fb4cbeb8a9364f244d
lemokami <jesswinw@gmail.com>
2022-06-08 22:40:49 +0530

    feat: add newForm and Form type for api purpose

--- interfaces/Form.ts
+++ interfaces/Form.ts
@@ -1,5 +1,18 @@
 import { RewardI } from './Reward';
 
+export type Form = {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  type: 'reward' | 'survey' | 'quiz' | 'form';
+  validity: string;
+  rewardId?: any;
+  userId: number;
+};
+export type NewForm = {
+  type: 'reward' | 'survey' | 'quiz' | 'form';
+  validity: Date;
+};
 type QuestionType =
   | {
       question: string;
