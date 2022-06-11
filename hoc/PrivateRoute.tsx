23412c6849594fe5da8cc4968eb37c304a461957
lemokami <jesswinw@gmail.com>
2022-06-11 21:08:11 +0530

    feat: working private route

--- hoc/PrivateRoute.tsx
+++ hoc/PrivateRoute.tsx
@@ -1,17 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 
 const PrivateRoute: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+  const [authorised, setAuthorised] = useState(false);
   useEffect(() => {
     // TODO: check token and authenticate
     const token = Cookies.get('token');
-    // if (!token) {
-    //   window.location.href = '/login';
-    // }
-  });
-  return <>{children}</>;
+    if (!token) {
+      window.location.href = '/login';
+    }
+    setAuthorised(true);
+  }, []);
+  return <>{authorised ? children : null}</>;
 };
 
 export default PrivateRoute;
