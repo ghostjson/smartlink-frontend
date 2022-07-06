17069b1653511615f803dbcaae41b608339d3f0e
lemokami <jesswinw@gmail.com>
2022-07-06 23:03:29 +0530

    feat: form submit succes page

---
+++ pages/submit.tsx
@@ -0,0 +1,12 @@
+import HomeLayout from '../components/layout/Home.layout';
+
+const Submit = () => {
+  return (
+    <div className='flex flex-col h-screen justify-center items-center'>
+      <div className='text-xl font-bold'>
+        Your response has been successfully recorded
+      </div>
+    </div>
+  );
+};
+export default Submit;
